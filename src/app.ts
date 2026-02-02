c733af815087a4325b745f9b7c7f6f96980b5d76
K1BERSTALKER <xabibullaev.dev@gmail.com>
2026-02-02 22:50:51 +0500

    final deploy

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,12 @@ import authRoutes from "@routes/authRoutes";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://auth-client-k1berstalkers-projects.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
